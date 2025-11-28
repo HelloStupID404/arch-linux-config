478c2e1a6b929434ef92fdb2b88da3388847fcb6
HelloStupID404 <2625491449@qq.com>
2025-11-28 09:10:39 +0800

    put tray on waybar

--- start/notes.bash
+++ start/notes.bash
@@ -1,8 +1,17 @@
 # Watch The Video: "https://www.youtube.com/watch?v=WaWB3F-ffcI&t=1263s"
 # These commands make me to reinstall ArchLinux faster.
 
+iwctl station wlan0 scan
+iwctl station wlan0 get-networks
+iwctl station wlan0 connect xiaomi13
+iwctl station wlan0 show
+
 nmcli dev wifi list
 nmcli dev wifi connect "xiaomi13" password 12345678
+
+sudo pacman -S bluez bluez-utils blueman
+sudo systemctl start bluetooth
+blueman-manager
 
 sudo pacman -S hyprlock hyprpaper hypridle hyprsunset hyprshot
 sudo pacman -S waybar btop yazi
@@ -29,6 +38,18 @@ sudo systemctl restart getty@tty1
 
 sudo pacman -S 7zip
 sudo pacman -S fd ripgrep
+sudo pacman -S ncdu
+
+pacman -Qdt
+sudo pacman -Scc
+sudo pacman -Rns $(pacman -Qdtq)
+sudo ncdu /
+
+sudo pacman -S power-profiles-daemon
+powerprofilesctl set performance
+
+git remote set-url origin git@github.com:HelloStupID404/arch-linux-config.git
 
 reboot
 poweroff
+sudo shutdown -h 10:30
